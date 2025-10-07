b958972fde0b4a918daf862b472b805c4187666b
Francisco_H_Lomas <franciscohenriquelomas@outlook.com>
2025-10-06 23:42:12 -0300

    Informacoes no readme.

---
+++ Automacoes/Steam_Trf.bat
@@ -0,0 +1,3 @@
+cd "C:\Pentaho\data-integration\"
+call Pan.bat /file="C:\Pentaho\data-integration\PentahoETL\steam.ktr" /level:Basic >> C:\Pentaho\data-integration\PentahoETL\steam_log_trf.txt
+exit
